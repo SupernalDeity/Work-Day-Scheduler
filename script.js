376aed3bb83f46d29389d65dc23930b7701a6cf1
Anthony <perezant121@gmail.com>
2022-08-25 16:36:05 -0400

    added a way to save todo text using local storage

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ todaysDate.innerHTML = moment().format('dddd, MMMM Do');
 }
 
 setDate();
+
+$('.container').on('click', 'button', function() {
+    var hourId = $(this).parent().attr('data-id');
+    var todoText = $(this).prev().val();
+    localStorage.setItem(hourId, todoText);
+});
+
+$('.todo').each(function() {
+    var hourId = $(this).parent().attr('data-id');
+    var todoText = localStorage.getItem(hourId);
+    $(this).val(todoText);
+});
