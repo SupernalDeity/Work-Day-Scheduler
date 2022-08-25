946853066c12c2793d2b35bbf38ebea247f2269d
Anthony <perezant121@gmail.com>
2022-08-25 17:35:48 -0400

    added background colors depending on the time

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var todaysDate = document.querySelector('#currentDay');
+var currentTime = moment().hour();
 
 var setDate = function () {
 todaysDate.innerHTML = moment().format('dddd, MMMM Do');
@@ -9,11 +10,23 @@ setDate();
 $('.container').on('click', 'button', function() {
     var hourId = $(this).parent().attr('data-id');
     var todoText = $(this).prev().val();
+
     localStorage.setItem(hourId, todoText);
 });
 
 $('.todo').each(function() {
     var hourId = $(this).parent().attr('data-id');
     var todoText = localStorage.getItem(hourId);
+
     $(this).val(todoText);
+
+    if(hourId < currentTime) {
+        $(this).addClass('bg-secondary');
+    };
+    if(hourId == currentTime) {
+        $(this).addClass('bg-danger')
+    }
+    if(hourId > currentTime) {
+        $(this).addClass('bg-success')
+    }
 });
